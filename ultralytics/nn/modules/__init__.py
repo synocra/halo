ef9f30c5e5b0567b6257459dbe256af934669948
synocra <mukhlisnurhidayat09@gmail.com>
2026-05-19 23:10:23 +0700

    push dataset

--- ultralytics/nn/modules/__init__.py
+++ ultralytics/nn/modules/__init__.py
@@ -59,6 +59,7 @@ from .block import (
     ResNetLayer,
     SCDown,
     TorchVision,
+    CoordAtt,
 )
 from .conv import (
     CBAM,
@@ -187,4 +188,5 @@ __all__ = (
     "YOLOESegment",
     "YOLOESegment26",
     "v10Detect",
+    "CoordAtt",
 )
